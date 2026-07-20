bbce2188607d6c009c49ee2a4222425366ecdee5
NamNT97 <thanhnam2k27@gmail.com>
2026-07-20 10:48:54 +0700

    Initial Commit

---
+++ QuickChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickChat
+//
+//  Created by NamNT97 on 20/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickChat/QuickChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickChatApp.swift
+//  QuickChat
+//
+//  Created by NamNT97 on 20/7/26.
+//
+
+import SwiftUI
+
+@main
+struct QuickChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
